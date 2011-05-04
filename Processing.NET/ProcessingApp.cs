4f210efdd4ea90772d02fcf76e9471e25a7b0540
Callum Rogers <callumrogers@hotmail.co.uk>
2011-05-04 11:35:29 +0100

    ProcessingApp basic structure

---
+++ Processing.NET/ProcessingApp.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+
+namespace Processing.NET
+{
+    public abstract class ProcessingApp
+    {
+        protected int Width { get; set; }
+        protected int Height { get; set; }
+
+        protected Color Background { get; set; }
+        protected Color Stroke { get; set; }
+
+
+        protected ProcessingApp()
+        {
+            InitialSetup();
+        }
+
+
+        private void InitialSetup()
+        {
+            Width = 100;
+            Height = 100;
+            Background = Color.LightGray;
+            Stroke = Color.Black;
+        }
+
+        protected abstract void Setup();
+        protected abstract void Draw();
+    }
+}
